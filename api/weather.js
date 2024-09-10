510edfbcb03ab422b175ad46c7292b055379b12e
Shedcrown <125031191+axdtora@users.noreply.github.com>
2024-09-10 20:47:47 +0300

    Add files via upload

---
+++ api/weather.js
@@ -0,0 +1,33 @@
+import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
+import { printHelp, printError, printSuccess } from './services/log.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не переданг token');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
+		printSuccess('Токен сохранен');
+	} catch(e) {
+		printError(e.message);
+	}
+}
+
+const initCLI = () => {
+	const args = getArgs(process.argv)
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+ 
+	}
+	if (args.t) {
+		return saveToken(args.t);
+	}
+	getWeather('Moscow')
+};
+
+initCLI();
